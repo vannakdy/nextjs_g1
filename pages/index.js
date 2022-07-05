3d562606a16af5e310108ee1d63d21de3017b33d
vannakdy <dyvannak1997@gmail.com>
2022-07-05 22:09:06 +0700

    slider

--- pages/index.js
+++ pages/index.js
@@ -1,85 +1,84 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Index.module.css';
-import Button from './component/button/Button';
-import {useState} from "react";
-import LayouteOne from './component/layout/LayouteOne';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Index.module.css";
+import Button from "./component/button/Button";
+import { useState } from "react";
+import LayouteOne from "./component/layout/LayouteOne";
+import HomeSlider from "./component/home/HomeSlider";
 
 const Home = () => {
   var arr = [
     {
-      id : 101,
-      name :  "Dell",
-      catgory : "Computer",
-      color : "black",
-      price : 1000
+      id: 101,
+      name: "Dell",
+      catgory: "Computer",
+      color: "black",
+      price: 1000,
     },
     {
-      id : 102,
-      name :  "Mac",
-      catgory : "Computer",
-      color : "black",
-      price : 1000
+      id: 102,
+      name: "Mac",
+      catgory: "Computer",
+      color: "black",
+      price: 1000,
     },
     {
-      id : 103,
-      name :  "Lenevo",
-      catgory : "Computer",
-      color : "black",
-      price : 1000
-    }
-  ]
+      id: 103,
+      name: "Lenevo",
+      catgory: "Computer",
+      color: "black",
+      price: 1000,
+    },
+  ];
 
-  const [value1,setValue1] = useState(0);
-  const [listProdcut,setListProduct] = useState(arr)
+  const [value1, setValue1] = useState(0);
+  const [listProdcut, setListProduct] = useState(arr);
 
   var value2 = 0;
   value2 = 100;
   const PI = 3.14;
 
   const myFunction = (name) => {
-      return name
-  }
+    return name;
+  };
   const handleClickBtn = () => {
-    setValue1(1000)
-    value2 = 1000
-  }
+    setValue1(1000);
+    value2 = 1000;
+  };
 
-  const handleClickMe = () => {
-
-  }
+  const handleClickMe = () => {};
 
   return (
     <LayouteOne>
+      <HomeSlider />
       <div className={styles.container}>
-
-      <Button
-        title={"Click ME"}
-        onClick={handleClickMe}
-      />
-      <h1 style={{color:'red',paddingBottom:100}}>value1 : {value1}</h1>
-      <h1>value2 : {value2}</h1>
-      <h1>{PI}</h1>
-      <h1>{myFunction("Jon")}</h1>
-      <h1>Hello Next.js</h1>
-      <button onClick={handleClickBtn}>Click me</button><br/>
-      {listProdcut.map((item,index)=>{
-        return (
-          <div 
-            key={index}
-            style={{
-              display : "flex",
-              marginBottom : 10
-            }}
-          >
-              <div style={{
-                width : 90,
-                height :110,
-                backgroundColor :"gray"
-              }}/>
+        <Button title={"Click ME"} onClick={handleClickMe} />
+        <h1 style={{ color: "red", paddingBottom: 100 }}>value1 : {value1}</h1>
+        <h1>value2 : {value2}</h1>
+        <h1>{PI}</h1>
+        <h1>{myFunction("Jon")}</h1>
+        <h1>Hello Next.js</h1>
+        <button onClick={handleClickBtn}>Click me</button>
+        <br />
+        {listProdcut.map((item, index) => {
+          return (
+            <div
+              key={index}
+              style={{
+                display: "flex",
+                marginBottom: 10,
+              }}
+            >
               <div
                 style={{
-                  paddingLeft:15
+                  width: 90,
+                  height: 110,
+                  backgroundColor: "gray",
+                }}
+              />
+              <div
+                style={{
+                  paddingLeft: 15,
                 }}
               >
                 <div>{item.name}</div>
@@ -87,11 +86,11 @@ const Home = () => {
                 <div>Color : {item.color}</div>
                 <div>Price : {item.price}</div>
               </div>
-          </div>
-        )
-      })}
-    </div>
+            </div>
+          );
+        })}
+      </div>
     </LayouteOne>
-  )
-}
+  );
+};
 export default Home;
